54dc799ba47f7a61bbab8e2de8d1abc44f81b72c
Vishal7017 <wavhalev@gmail.com>
2019-03-09 20:14:47 +0530

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { ContentComponent } from './content/content.component';
+import { ClientComponent } from './client/client.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FooterComponent } from './footer/footer.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    GallaryComponent,
+    ContentComponent,
+    ClientComponent,
+    TestimonialsComponent,
+    FooterComponent,
+    PricingComponent,
+    HeaderComponent,
+    SocialComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
